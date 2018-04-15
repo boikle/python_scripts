33ed469100dd94f4d7b2fbf11573952acb672654
Robert Oikle <6431161+roikle@users.noreply.github.com>
2018-04-15 09:39:05 -0400

    Initial commit for nunaliit csv import script

---
+++ other_scripts/nunaliit_csv_import/nunaliit_csv_import.py
@@ -0,0 +1,37 @@
+import couchdb
+import csv
+
+# Required Config information for connecting to the couchDb
+couchDbURL = "URL location, e.g. 127.0.0.1"
+couchDbPort = "port number, e.g. 5984"
+couchDbUserName = "Your user name, e.g. admin"
+couchDbUserPass = "Your password"
+couchDbName = "Name of your couchDb database"
+# Location of file being imported
+fileName = "location of the csv file, e.g. data/mydata.csv"
+# Assigned schema for import
+schemaName = "nunaliit schema name"
+
+# Connect to CouchDB
+couch = couchdb.Server('http://' + couchDbUserName + ':' + couchDbUserPass + '@' + couchDbURL + ':' + couchDbPort + '/')
+db = couch[couchDbName]
+
+print( "Starting CSV import ..." )
+
+# Walk through CSV file rows
+with open(fileName, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    
+    for row in reader:
+        doc = {}
+        # Add schema name
+        doc['nunaliit_schema'] = schemaName
+        rowContent = {}
+        for keyValuePair in row.items():
+            key = keyValuePair[0]
+            value = keyValuePair[1]
+            rowContent[key] = value
+        doc[schemaName] = rowContent        
+        db.save(doc)
+
+print( "CSV import complete" )
