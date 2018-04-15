30e2db894ce297559b843602223ac7b3bedf93fe
Robert Oikle <6431161+roikle@users.noreply.github.com>
2018-04-15 09:30:45 -0400

    Divided burn scar functions and nbr functions into separate classes

--- spatial_scripts/burn_scar_analysis/burnScarAnalysis.py
+++ spatial_scripts/burn_scar_analysis/burnScarAnalysis.py
@@ -11,61 +11,68 @@ except:
     sys.exit("Error: can't find required numpy module")
 
 class burnScarAnalysis:
-    def __init__(self, *args):
+    def __init__(self):
         self.outDriver = gdal.GetDriverByName("GTiff")
-        self.preRedBand = gdal.Open(sys.argv[1])
-        self.preRedBandArray = self.preRedBand.ReadAsArray()
-        self.preNIRBand = gdal.Open(sys.argv[2])
-        self.preNIRBandArray = self.preNIRBand.ReadAsArray()
-        self.outputFile = sys.argv[3]
-        self.maskValue = -9999
-    
-    def _closeDatasets(self):
-        print("Closing Datasets ...")
-        self.preRedBand = None
-        self.preNIRBand = None
 
-    def _outputRaster(self, nbrArray):
-        print("Outputing Normalized Burn Ratio raster ...")
-        outputRaster = self.outDriver.Create(str(self.outputFile), self.rows, self.cols, 1, gdal.GDT_Float32)
+    def _outputRaster(self, inputArray, outputRaster):
+        print("Outputing raster result...")
+        outputRaster = self.outDriver.Create(str(outputRaster), self.rows, self.cols, 1, gdal.GDT_Float32)
         
         # write NBRarray to output file
-        outputRaster.GetRasterBand(1).WriteArray(nbrArray)
+        outputRaster.GetRasterBand(1).WriteArray(inputArray)
         outputRaster.SetGeoTransform(self.geoTransform)
         outputRaster.SetProjection(self.projection)
 
         # Close output raster
         outputRaster = None
 
-    def _getImgSpecs(self):
+    def _getImgSpecs(self, raster):
         print("Getting Image Specifications ...")
-        self.geoTransform = self.preRedBand.GetGeoTransform()
-        self.projection = self.preRedBand.GetProjection()
-        [self.cols,self.rows] = self.preRedBandArray.shape
+        inputRaster = gdal.Open(raster)
+        self.geoTransform = inputRaster.GetGeoTransform()
+        self.projection = inputRaster.GetProjection()
+        [self.cols,self.rows] = inputRaster.ReadAsArray().shape
+
+        # Close input raster
+        inputRaster = None
+
+class nbr(burnScarAnalysis):
+    def __init__(self, *args):
+        burnScarAnalysis.__init__(self)
+        self.redBand = gdal.Open(sys.argv[1])
+        self.redBandArray = self.redBand.ReadAsArray()
+        self.nirBand = gdal.Open(sys.argv[2])
+        self.nirBandArray = self.nirBand.ReadAsArray()
+        self.outputRaster = sys.argv[3]
+        self.maskValue = -9999
+    
+    def _closeDatasets(self):
+        print("Closing Datasets ...")
+        self.redBand = None
+        self.nirBand = None
 
     # Create a mask indicating invalid pixels
     # - Mask out invalid Landsat 8 surface reflectance values (< 0 | > 10000)
     # - Mask out when denominator equals 0
     def _createMask(self):
         print("Creating Mask ...")
-        self.mask = numpy.where((self.preRedBandArray < 0) 
-        | (self.preNIRBandArray < 0) 
-        | (self.preRedBandArray > 10000) 
-        | (self.preNIRBandArray > 10000) 
-        | (self.preRedBandArray + self.preNIRBandArray == 0), 0, 1)
+        self.mask = numpy.where((self.redBandArray < 0) 
+        | (self.nirBandArray < 0) 
+        | (self.redBandArray > 10000) 
+        | (self.nirBandArray > 10000) 
+        | (self.redBandArray + self.nirBandArray == 0), 0, 1)
 
     # Normalized Burn Ratio 
     # NBR = (Red - NIR)/(Red + NIR)
     def _createNBR(self):
         print("Creating Normalized Burn Ratio ...")
         NBRArray = numpy.choose(self.mask,(self.maskValue, 
-        (self.preRedBandArray - self.preNIRBandArray)/
-        (self.preRedBandArray + self.preNIRBandArray)))
-        self._outputRaster(NBRArray)
+        (self.redBandArray - self.nirBandArray)/
+        (self.redBandArray + self.nirBandArray)))
+        self._outputRaster(NBRArray,self.outputRaster)
 
-
-burnScar = burnScarAnalysis(sys.argv[1], sys.argv[2], sys.argv[3])
-burnScar._getImgSpecs()
-burnScar._createMask()
-burnScar._createNBR()
-burnScar._closeDatasets()
+preFireNBR = nbr(sys.argv[1], sys.argv[2], sys.argv[3])
+preFireNBR._getImgSpecs(sys.argv[1])
+preFireNBR._createMask()
+preFireNBR._createNBR()
+preFireNBR._closeDatasets()
