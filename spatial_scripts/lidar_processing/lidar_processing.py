64381ec44d6ede951e164a8d54d771e30428c18c
Robert Oikle <6431161+roikle@users.noreply.github.com>
2019-07-27 23:55:03 -0400

    Updated delete work dirs function and added create ground tiles function

--- spatial_scripts/lidar_processing/lidar_processing.py
+++ spatial_scripts/lidar_processing/lidar_processing.py
@@ -7,11 +7,15 @@ HOME_DIR = os.getenv("HOME")
 DATA_DIR = HOME_DIR + '/Desktop/lidar_processing/data/'
 LASTOOLS_DIR = HOME_DIR + '/LAStools/bin/'
 
-def delete_tiles():
-    '''Delete old tiles'''
+def del_work_dirs():
+    '''Delete old work directories'''
     print('Deleting old tile data ...')
     os.system('rm -r ' + DATA_DIR + 'tiles')
     os.system('mkdir ' + DATA_DIR + 'tiles')
+    print('Deleting old ground data ...')
+    os.system('rm -r ' + DATA_DIR + 'ground')
+    os.system('mkdir ' + DATA_DIR + 'ground')
+
 
 def create_lidar_tiles():
     '''Create lidar tiles for each laz file'''
@@ -22,8 +26,20 @@ def create_lidar_tiles():
             os.system('wine ' + LASTOOLS_DIR + 'lastile -i "' + file_path
                       + '" -tile_size 500 -buffer 25 -o ' + tile_path)
 
-# Delete old tiles
-delete_tiles()
+def create_ground_tiles():
+    '''Create Lidar ground laz fils'''
+    for filename in os.listdir(DATA_DIR + 'tiles/'):
+        if filename.endswith('.laz') or filename.endswith('.las'):
+            file_path = os.path.join(DATA_DIR + 'tiles/', filename)
+            ground_path = os.path.join(DATA_DIR + 'ground/', filename)
+            os.system('wine ' + LASTOOLS_DIR + 'lasground -i "' + file_path
+                      + '" -o ' + ground_path)
+
+# Delete old work directories
+del_work_dirs()
 
 # Create lidar tiles
 create_lidar_tiles()
+
+# Create lidar ground files
+create_ground_tiles()
