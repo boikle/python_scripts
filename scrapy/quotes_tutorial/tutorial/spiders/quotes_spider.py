5e22eeff4a3a612b57ab525c2e371d652185a425
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-01-04 18:44:44 -0500

    Added support for following urls for scrapying new data

--- scrapy/quotes_tutorial/tutorial/spiders/quotes_spider.py
+++ scrapy/quotes_tutorial/tutorial/spiders/quotes_spider.py
@@ -2,17 +2,25 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
+    # Name of the spider
     name = "quotes"
 
+    # URL's to scrape
     start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
+        'http://quotes.toscrape.com/page/1/'
     ]
 
     def parse(self, response):
+        # Extract the quote, author, and tags for each quote entry
         for quote in response.css('div.quote'):
             yield {
                 'text': quote.css('span.text::text').get(),
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall(),
             }
+
+        # Follow the next link if available
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
