ccac523c70bd970f5615aea021edcf1be3710a8b
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-02-03 19:51:15 -0500

    Added encrypt and get_ascii_value methods

--- other_scripts/caesar_cipher/caesar_cipher.py
+++ other_scripts/caesar_cipher/caesar_cipher.py
@@ -9,15 +9,41 @@ class CaesarCipher:
             except:
                 sys.exit("Must provide 'shift' amount, and 'message'")
 
-        self.encrypt()
+        print ('---------------------------------')
+        print('abcdefghijklmnopqrstuvwxyz')
+        print(self.encrypt('abcdefghijklmnopqrstuvwxyz'))
+        print ('---------------------------------')
+        print('Encrypted Message: {}'.format(self.encrypt(self.message)))
 
-    def encrypt(self):
-        print('Original Message: {}'.format(self.message))
-        print('Encrypted Message: ')
+    def get_ascii_value(self, char):
+        ascii_min = 97
+        ascii_max = 122
+        if (ord(char) >= ascii_min and ord(char) <= ascii_max):
+            shifted_char_ascii = ord(char) + int(self.shift)
+
+            if shifted_char_ascii < ascii_min:
+                return ascii_max - (ascii_min - shifted_char_ascii - 1)
+            elif shifted_char_ascii > ascii_max:
+                return ascii_min + (shifted_char_ascii - ascii_max - 1)
+            else:
+                return shifted_char_ascii
+        else:
+            return ord(char)
+
+    def encrypt(self, msg):
+        encrypted_msg = ""
+        for char in msg.lower():
+
+            shifted_char = self.get_ascii_value(char)
+            encrypted_msg += chr(shifted_char)
+
+        return encrypted_msg
+
+
 
 print('Caesar Cipher')
 print('------------------------------')
-shiftAmt = input('Shift Amount: ')
+shift_amt = input('Shift Amount (-25 to 25): ')
 msg = input('Message: ')
 
-CaesarCipher(shift=shiftAmt, message=msg)
+CaesarCipher(shift=shift_amt, message=msg)
