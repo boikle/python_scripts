b4395278c619afb6995e0808db0d14504ccf0116
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-11-06 14:26:39 -0500

    Tweak to drop rows with null column script

--- other_scripts/pandas_drop_null_columns/pandas_drop_null_columns.py
+++ other_scripts/pandas_drop_null_columns/pandas_drop_null_columns.py
@@ -7,11 +7,11 @@ output_file = "output.csv"
 fields = ['latitude', 'longitude']
 
 # Create a Pandas data frame
-df = pd.read_csv(input_file, index_col=0, encoding='utf-8')
+df = pd.read_csv(input_file, index_col=None, encoding='utf-8')
 
 # Loop through fields that need to be not null
 for field in fields:
     df = df[~df[field].isnull()]
 
 # Output results
-df.to_csv(output_file)
+df.to_csv(output_file, index=False)
