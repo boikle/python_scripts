811c3f32e702c339a4c353f7171bf0e1a7076cfd
Robert Oikle <6431161+roikle@users.noreply.github.com>
2020-12-27 08:10:18 -0500

    Updated script to extract data from greek cities table

--- scraping/greek_cities/greek_cities.py
+++ scraping/greek_cities/greek_cities.py
@@ -25,18 +25,35 @@ def main():
     """
     Main entry to scrapping script
     """
-    data_source_url = "https://en.wikipedia.org/wiki/Category:Roman_towns_and_cities_by_country"
+
+    data_source_url = "https://en.wikipedia.org/wiki/List_of_ancient_Greek_cities"
     src_content = getSourceContent(data_source_url)
 
     soup = BeautifulSoup(src_content, 'lxml')
 
-    # Get list of Roman Town and City Links
-    links = soup.select('.mw-category .CategoryTreeItem a')
+    # Get list of Greek City Links
+    links = soup.select('#mw-content-text table.wikitable tr td:first-child a')
 
     print(links)
     for link in links:
+        host_url = 'https://en.wikipedia.org'
+        city_url = host_url + link.attrs['href']
+        city_name = link.text
+        city_coords = get_city_coords(city_url)
+        if city_coords:
+            print("{}, {}".format(city_name, city_coords))
 
-        print('https://en.wikipedia.org' + link.attrs['href'])
+
+def get_city_coords(city_url):
+   coords = None
+   city_source = getSourceContent(city_url)
+   soup = BeautifulSoup(city_source, 'lxml')
+   geodec = soup.find('span', class_='geo-dec')
+
+   if geodec and geodec.text:
+        coords = geodec.text
+
+   return coords
 
 if __name__ == '__main__':
     main()
