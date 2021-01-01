f03e769d31d47fa4f5a00c316d1bfcef27f90312
Robert Oikle <6431161+roikle@users.noreply.github.com>
2021-01-01 06:44:13 -0500

    Moved get_source_content function to utils module

--- scraping/ancient_sites/utils.py
+++ scraping/ancient_sites/utils.py
@@ -2,6 +2,31 @@ import csv
 import os
 
 
+def get_source_content(source_url):
+    """
+    Get the HTML content for the provided source URL
+
+    Attributes:
+    ---------------
+    source_url: string
+        The URL for the website being requested
+
+    Returns:
+    ---------------
+    src: bytes literal
+        A string containing the html content for the provided source
+    """
+    src = None
+    try:
+        requested_data = requests.get(source_url, timeout=1000)
+        src = requested_data.content
+    except requests.exceptions.ConnectionError:
+        print("Connection refused")
+
+
+    return src
+
+
 def export_data(file_name, fields, rows):
     """
     Export scrapped data
@@ -19,6 +44,10 @@ def export_data(file_name, fields, rows):
     # Create output directory if it doesn't exist
     if not os.path.exists('output/'):
         os.mkdir('output')
+
+    # Delete old output file if it exists
+    if os.path.exists('output/' + file_name):
+        os.remove('output/' + file_name)
 
     with open('output/' + file_name, 'w') as f:
         write = csv.writer(f)
