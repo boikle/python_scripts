40ac86d729ba0fe1663891e30f7c0e5ed8f4f184
Robert Oikle <6431161+roikle@users.noreply.github.com>
2021-01-03 01:44:08 -0500

    Moved coordinate scrapping helper functions to utils module

--- scraping/ancient_sites/greek_cities.py
+++ scraping/ancient_sites/greek_cities.py
@@ -3,61 +3,6 @@ from bs4 import BeautifulSoup
 import utils
 
 
-def get_city_coords(city_url):
-    """
-    Get city coordinates string from wikipedia page
-    """
-    coords = None
-    city_source = utils.get_source_content(city_url)
-    if city_source:
-        soup = BeautifulSoup(city_source, 'lxml')
-        geodec = soup.find('span', class_='geo-dec')
-
-        if geodec and geodec.text:
-            coords = geodec.text
-
-    return coords
-
-
-def parse_lat(coords):
-    """
-    Parse lat/long string to retrieve latitude number in decimal degree format
-
-    Example: 40.2°N 35.5°E  returns 40.2
-    """
-    lat = None
-
-    if coords.find("°N") != -1:
-        lat_index = coords.index("°N")
-        lat = float(coords[:lat_index])
-    elif coords.find("°S") != -1:
-        lat_index = coords.index("°S")
-        lat = float(coords[:lat_index]) * -1
-
-    return lat
-
-
-def parse_long(coords):
-    """
-    Parse lat/long string to retrieve longitude number in decimal degree format
-    
-    Example: 40.2°N 35.5°E  returns 35.5
-    """
-    long = None
-    coords = coords.split()
-
-    if len(coords) == 2:
-        coords = coords[1]
-        if coords.find("°E") != -1:
-            long_index = coords.index("°E")
-            long = float(coords[:long_index])
-        elif coords.find("°W") != -1:
-            long_index = coords.index("°W")
-            long = float(coords[:long_index]) * -1
-
-    return long
-
-
 def main():
     """
     Main entry to scrapping script
@@ -78,10 +23,10 @@ def main():
         host_url = 'https://en.wikipedia.org'
         city_url = host_url + link.attrs['href']
         city_name = link.text
-        city_coords = get_city_coords(city_url)
+        city_coords = utils.get_city_coords(city_url)
         if city_coords:
-            print("{}, {}, {}".format(city_name, parse_lat(city_coords), parse_long(city_coords)))
-            rows.append([city_name, parse_lat(city_coords), parse_long(city_coords)])
+            print("{}, {}, {}".format(city_name, utils.parse_lat(city_coords), utils.parse_long(city_coords)))
+            rows.append([city_name, utils.parse_lat(city_coords), utils.parse_long(city_coords)])
 
     # Export scraped data
     utils.export_data('greek_cities.csv', fields, rows)
--- scraping/ancient_sites/utils.py
+++ scraping/ancient_sites/utils.py
@@ -27,6 +27,91 @@ def get_source_content(source_url):
     return src
 
 
+def get_city_coords(city_url):
+    """
+    Get city coordinates string from wikipedia page
+
+    Attributes:
+    ---------------
+    city_url: string
+        The URL for the website being requested
+
+    Returns:
+    ---------------
+    coords: string
+        A string containing the Lat/Long coordinates
+    """
+    coords = None
+    city_source = utils.get_source_content(city_url)
+    if city_source:
+        soup = BeautifulSoup(city_source, 'lxml')
+        geodec = soup.find('span', class_='geo-dec')
+
+        if geodec and geodec.text:
+            coords = geodec.text
+
+    return coords
+
+
+def parse_lat(coords):
+    """
+    Parse lat/long string to retrieve latitude number in decimal degree format
+
+    Example: 40.2°N 35.5°E  returns 40.2
+
+    Attributes:
+    ---------------
+    coords: string
+        The lat/long coordinate string
+
+    Returns:
+    ---------------
+    lat: string
+        A string containing the decimal degree latitude number
+    """
+    lat = None
+
+    if coords.find("°N") != -1:
+        lat_index = coords.index("°N")
+        lat = float(coords[:lat_index])
+    elif coords.find("°S") != -1:
+        lat_index = coords.index("°S")
+        lat = float(coords[:lat_index]) * -1
+
+    return lat
+
+
+def parse_long(coords):
+    """
+    Parse lat/long string to retrieve longitude number in decimal degree format
+
+    Example: 40.2°N 35.5°E  returns 35.5
+
+    Attributes:
+    ---------------
+    coords: string
+        The lat/long coordinate string
+
+    Returns:
+    ---------------
+    long: string
+        A string containing the decimal degree latitude number
+    """
+    long = None
+    coords = coords.split()
+
+    if len(coords) == 2:
+        coords = coords[1]
+        if coords.find("°E") != -1:
+            long_index = coords.index("°E")
+            long = float(coords[:long_index])
+        elif coords.find("°W") != -1:
+            long_index = coords.index("°W")
+            long = float(coords[:long_index]) * -1
+
+    return long
+
+
 def export_data(file_name, fields, rows):
     """
     Export scrapped data
