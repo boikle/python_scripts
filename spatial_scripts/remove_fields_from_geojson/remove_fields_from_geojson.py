7d88f216c2cd9e6fb8e8523885c3d7f2e3b8bc23
Robert Oikle <6431161+roikle@users.noreply.github.com>
2021-01-11 14:10:26 -0500

    Script for removing specied fields from geospatial data

---
+++ spatial_scripts/remove_fields_from_geojson/remove_fields_from_geojson.py
@@ -0,0 +1,62 @@
+"""
+A geospatial script to remove specified fields to drop from spatial data.
+"""
+import fiona
+import sys
+
+
+def remove_fields_from_features(input_features, fields_to_drop):
+    """
+    Remove specified fields from spatial data
+
+    Attributes:
+    --------------------
+    :param input_features {string}: File location of the input data
+    :param fields_to_drop {string}: List of fields to be dropped from data. e.g. "a,b,c"
+    """
+    output_file = input_features.name + 'removed_fields.geojson'
+
+    with fiona.open(output_file, 'w', driver='GeoJSON', crs=input_features.crs, schema=input_features.schema) as output:
+
+        # Update input schema by removing dropped fields
+        for drop_field in fields_to_drop:
+            if 'properties' in output.schema:
+                properties = output.schema['properties']
+                if drop_field in properties:
+                    del properties[drop_field]
+
+            # Update output schema properties with changes
+            output.schema['properties'] = properties
+
+        # Update all geometries by removing dropped fields
+        for elem in input_features:
+            # Remove element properties
+            updated_elem_props = elem['properties']
+
+            for drop_field in fields_to_drop:
+                if drop_field in updated_elem_props:
+                    del updated_elem_props[drop_field]
+
+            # Output geometries with reduced coordinate precision
+            output.write({
+                'geometry': elem['geometry'],
+                'properties': updated_elem_props
+            })
+
+        print("Finished removing dropped fields from {}".format(input_features.name))
+
+def main(input_file, dropped_fields):
+    """
+    Entry point for removing fields from input data
+    """
+    print("Opening input data...")
+    with fiona.open(input_file) as input_features:
+        remove_fields_from_features(input_features, dropped_fields.split(','))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 3:
+        args = sys.argv
+        main(sys.argv[1], sys.argv[2])
+    else:
+        raise SyntaxError("Invalid number of arguments.")
